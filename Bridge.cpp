12b96bb61e607a3dd570d868d393b5fe277253dd
sh999 <sh999gh@gmail.com>
2016-09-29 17:08:46 -0400

    Update best port msg after every send

--- Bridge.cpp
+++ Bridge.cpp
@@ -9,6 +9,7 @@
 		bridge_id = stoi(s);
 		config_message = Config_Message(get_bridge_id(), 0, get_bridge_id());
 	}
+
 	void Bridge::connect_bridge_with_lans(string line){
 		istringstream line_stream(line);
 		string n;
@@ -21,32 +22,33 @@
 			counter++;
 		}
 	}
-	void Bridge::receive_message(Config_Message sender_message){
-		//CHECKPOINT:  deal w/ message
+
+	void Bridge::receive_message(Config_Message received_msg){		// Deal with message sent by another bridge
+		bool changed = false;
 		cout << "\n\t\t\tBridge " << get_bridge_id() << " ";
 		config_message.print_config();
 		cout << " has received message: ";
-		sender_message.print_config();
-		if(sender_message.get_root() < config_message.get_root()){
+		received_msg.print_config();
+		if(received_msg.get_root() < config_message.get_root()){		// If received message has lower root, set new root and root distance
 			cout << "\n\t\t\t\tSmaller root seen.  Changing root and distance\n";
-			config_message.set_root(sender_message.get_root());
-			config_message.set_dist_root(sender_message.get_dist_root()+1);
+			config_message.set_root(received_msg.get_root());
+			config_message.set_dist_root(received_msg.get_dist_root()+1);
 			cout << "\t\t\t\tSet config message to ";
 			config_message.print_config();
 			cout << endl;
+			changed = true;
 		}
-		else if (sender_message.get_root() == config_message.get_root()){
-			bool changed = false;
+		else if (received_msg.get_root() == config_message.get_root()){	// Else if same root but different dist to root or different sender #
 			// cout << "\n\t\t\t\tsame root seen\n";
-			if(sender_message.get_dist_root()+1 < config_message.get_dist_root()){
+			if(received_msg.get_dist_root()+1 < config_message.get_dist_root()){
 				cout << "\n\t\t\t\tDist+1 is smaller\n";
-				config_message.set_dist_root(sender_message.get_dist_root()+1);
+				config_message.set_dist_root(received_msg.get_dist_root()+1);
 				changed = true;
 			}
 			else{
 				changed = false;
 			}
-			if(changed){
+			if(changed){													// this boolean is for display purposes only
 				cout << "\n\t\t\t\tChanging config message to ";
 			}
 			else{
@@ -55,25 +57,38 @@
 			config_message.print_config();
 			cout << endl;
 		}
+		check_port(received_msg);
+	}
+	void Bridge::check_port(Config_Message received_msg){
+		int port = received_msg.get_sender();
+		cout << "\n\t\t\t\tChecking if msg "; received_msg.print_config(); 
+		cout << " is best for port " << port; best_msg_on_ports[port].print_config(); cout << endl;
+		if(received_msg.get_root() < best_msg_on_ports[port].get_root()){
+			best_msg_on_ports[port] = received_msg;
+			cout << "\t\t\t\tReceived msg is better than previous\n";
+		}
 	}
 	void Bridge::add_neighbors(Bridge b){
 		// Add neighboring bridges (having same lans) to current bridge
 		// other_bridges.push_back(b);
 		other_bridges.insert(b.get_bridge_id());
 		Config_Message c(999,999,999);
-		best_msg_on_port[b.get_bridge_id()] = c;
+		best_msg_on_ports[b.get_bridge_id()] = c;
 	}
 	int Bridge::get_bridge_id(){
 		return bridge_id;
 	}
+
 	void Bridge::send_bridge(){
 		cout << "\tSending from bridge"<< bridge_id << endl;
 		cout << "\t\tThis bridge's config:\n\t\t";
 		config_message.print_config();
 	}
+
 	Config_Message Bridge::get_message(){
 		return config_message;
 	}
+
 	void Bridge::print_bridge(){
 		cout << "Bridge #"<< bridge_id << ":";
 		config_message.print_config();
@@ -88,7 +103,7 @@
 		}
 		cout << "\tBest port msg's:\n";
 		typedef map<int, Config_Message>::iterator it;
-		for(it i = best_msg_on_port.begin(); i != best_msg_on_port.end(); i++){
+		for(it i = best_msg_on_ports.begin(); i != best_msg_on_ports.end(); i++){
 			cout << "\t\tFrom port/link to bridge " << i->first << ":";
 			i->second.print_config();
 			cout << endl;
--- Bridge.h
+++ Bridge.h
@@ -14,12 +14,13 @@ struct Bridge{
 	int bridge_id;
 	vector<string> lans; 			// What LANs are linked to this bridge
 	set<int> other_bridges; 		// What other bridges are linked to this bridge 
-	map<int, Config_Message> best_msg_on_port; 	// Best Config message (value) on port int (key)
+	map<int, Config_Message> best_msg_on_ports; 	// Best Config message (value) on port int (key)
 	Config_Message config_message;
 	Bridge(string line);
 	void connect_bridge_with_lans(string line);
 	void add_neighbors(Bridge);
 	void receive_message(Config_Message m);
+	void check_port(Config_Message m);		// Check if received msg is better for a given port/link
 	int get_bridge_id();
 	void send_bridge();
 	void print_bridge();
